5685e0ad03581ba8ac855467561aaee0fea25cb9
nilly34 <112898679+nilly34@users.noreply.github.com>
2022-12-08 22:19:35 +0000

    Update NewBranch1.java

--- src/NewBranch/NewBranch1.java
+++ src/NewBranch/NewBranch1.java
@@ -4,5 +4,8 @@ public class NewBranch1 {
     public static void main(String[] args) {
         String word="Wooden spoon";
         System.out.println(word);
+        
+        int new=123;
+        System.out.println(new);
     }
 }
